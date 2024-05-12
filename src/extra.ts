4248d6f14efd5951726c541907388321d9201ce6
Sercrac07 <sercassan@gmail.com>
2024-05-12 17:41:49 +0200

    Fixed info bug

--- src/extra.ts
+++ src/extra.ts
@@ -15,7 +15,7 @@ const regex = new RegExp(`.{1,${stdout.columns - 4}}`, 'g')
 export function intro(message: string) {
   const splittedMessage = message.match(regex)!
 
-  stdout.write(`${Colors.FgGreen + Symbols.TopLeftCorner + Symbols.LineHorizontal.repeat(splittedMessage[0].length + 2) + Symbols.TopRightCorner}\n${splittedMessage.map(msg => `${Symbols.LineVertical + Colors.Reset} ${Colors.Dim + msg.padEnd(stdout.columns - 4, ' ') + Colors.Reset} ${Colors.FgGreen + Symbols.LineVertical}`).join('')}\n${Symbols.MiddleLine + Symbols.LineHorizontal.repeat(splittedMessage[0].length + 2) + Symbols.BottomRightCorner}\n${Symbols.LineVertical + Colors.Reset}\n`)
+  stdout.write(`${Colors.FgGreen + Symbols.TopLeftCorner + Symbols.LineHorizontal.repeat(splittedMessage[0].length + 2) + Symbols.TopRightCorner}\n${splittedMessage.map(msg => `${Symbols.LineVertical + Colors.Reset} ${Colors.Dim + msg.padEnd(splittedMessage[0].length, ' ') + Colors.Reset} ${Colors.FgGreen + Symbols.LineVertical}`).join('')}\n${Symbols.MiddleLine + Symbols.LineHorizontal.repeat(splittedMessage[0].length + 2) + Symbols.BottomRightCorner}\n${Symbols.LineVertical + Colors.Reset}\n`)
 }
 
 /**
@@ -28,7 +28,7 @@ export function intro(message: string) {
 export function outro(message: string) {
   const splittedMessage = message.match(regex)!
 
-  stdout.write(`${Colors.FgGreen + Symbols.MiddleLine + Symbols.LineHorizontal.repeat(splittedMessage[0].length + 2) + Symbols.TopRightCorner}\n${splittedMessage.map(msg => `${Symbols.LineVertical + Colors.Reset} ${Colors.Dim + msg.padEnd(stdout.columns - 4, ' ') + Colors.Reset} ${Colors.FgGreen + Symbols.LineVertical}`).join('')}\n${Symbols.BottomLeftCorner + Symbols.LineHorizontal.repeat(splittedMessage[0].length + 2) + Symbols.BottomRightCorner + Colors.Reset}\n`)
+  stdout.write(`${Colors.FgGreen + Symbols.MiddleLine + Symbols.LineHorizontal.repeat(splittedMessage[0].length + 2) + Symbols.TopRightCorner}\n${splittedMessage.map(msg => `${Symbols.LineVertical + Colors.Reset} ${Colors.Dim + msg.padEnd(splittedMessage[0].length, ' ') + Colors.Reset} ${Colors.FgGreen + Symbols.LineVertical}`).join('')}\n${Symbols.BottomLeftCorner + Symbols.LineHorizontal.repeat(splittedMessage[0].length + 2) + Symbols.BottomRightCorner + Colors.Reset}\n`)
 }
 
 /**
@@ -41,5 +41,5 @@ export function outro(message: string) {
 export function info(message: string) {
   const splittedMessage = message.match(regex)!
 
-  stdout.write(`${Colors.FgGreen + Symbols.MiddleLine + Symbols.LineHorizontal.repeat(splittedMessage[0].length + 2) + Symbols.TopRightCorner}\n${splittedMessage.map(msg => `${Symbols.LineVertical + Colors.Reset} ${Colors.Dim + msg.padEnd(stdout.columns - 4, ' ') + Colors.Reset} ${Colors.FgGreen + Symbols.LineVertical}`).join('')}\n${Symbols.MiddleLine + Symbols.LineHorizontal.repeat(splittedMessage[0].length + 2) + Symbols.BottomRightCorner}\n${Symbols.LineVertical + Colors.Reset}\n`)
+  stdout.write(`${Colors.FgGreen + Symbols.MiddleLine + Symbols.LineHorizontal.repeat(splittedMessage[0].length + 2) + Symbols.TopRightCorner}\n${splittedMessage.map(msg => `${Symbols.LineVertical + Colors.Reset} ${Colors.Dim + msg.padEnd(splittedMessage[0].length, ' ') + Colors.Reset} ${Colors.FgGreen + Symbols.LineVertical}`).join('')}\n${Symbols.MiddleLine + Symbols.LineHorizontal.repeat(splittedMessage[0].length + 2) + Symbols.BottomRightCorner}\n${Symbols.LineVertical + Colors.Reset}\n`)
 }
