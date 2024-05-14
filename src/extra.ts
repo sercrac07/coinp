c05cf09aec65fa8f7e798a34b179be92fb4978dd
Sercrac07 <sercassan@gmail.com>
2024-05-14 22:35:26 +0200

    Added title to extra functions

--- src/extra.ts
+++ src/extra.ts
@@ -5,6 +5,7 @@ import { Colors, Symbols, Unicode } from './lib/consts'
 import { getLargestStringLength } from './lib/arrays'
 
 const regex = new RegExp(`.{1,${stdout.columns - 4}}`, 'g')
+const regexTitle = new RegExp(`.{1,${stdout.columns - 6}}`, 'g')
 
 /**
  * The intro function allows you to display an introductory message with embellished information at the beginning of an application or section.
@@ -13,11 +14,15 @@ const regex = new RegExp(`.{1,${stdout.columns - 4}}`, 'g')
  * intro('Hello world')
  * ```
  */
-export function intro(...message: string[]) {
-  const splittedMessage = message.map(mes => mes.trim().match(regex)!)
-  const largestString = getLargestStringLength(splittedMessage.flatMap(str => str))
+export function intro(title: string, ...message: string[]) {
+  const splittedTitle = title.trim().match(regexTitle)!
 
-  stdout.write(`${Colors.FgGreen + Symbols.TopLeftCorner + Symbols.LineHorizontal.repeat(largestString + 2) + Symbols.TopRightCorner}\n${splittedMessage.map(msg => msg.map(m => `${Symbols.LineVertical + Colors.Reset} ${Colors.Dim + m.padEnd(largestString, ' ') + Colors.Reset} ${Colors.FgGreen + Symbols.LineVertical}`).join('')).join('\n')}\n${Symbols.MiddleLine + Symbols.LineHorizontal.repeat(largestString + 2) + Symbols.BottomRightCorner}\n${Symbols.LineVertical + Colors.Reset}\n`)
+  const splittedMessage = message.map(mes => mes.trim().match(regex)!)
+  const largestString = getLargestStringLength([...splittedMessage.flatMap(str => str), ...splittedTitle])
+
+  stdout.write(`${splittedTitle.map((ttl, index) => `${Colors.FgGreen}${index === 0 ? Symbols.TopLeftCorner : Symbols.LineVertical} ${Colors.Reset + Colors.Reverse} ${ttl} ${Colors.Reset + Colors.FgGreen} ${index === 0 ? Symbols.LineHorizontal.repeat(largestString - ttl.length) + Symbols.TopRightCorner : ' '.repeat(largestString - ttl.length) + Symbols.LineVertical}`).join('')}\n`)
+
+  stdout.write(`${splittedMessage.map(msg => msg.map(m => `${Symbols.LineVertical + Colors.Reset} ${Colors.Dim + m.padEnd(largestString + 2, ' ') + Colors.Reset} ${Colors.FgGreen + Symbols.LineVertical}`).join('')).join('\n')}\n${Symbols.MiddleLine + Symbols.LineHorizontal.repeat(largestString + 4) + Symbols.BottomRightCorner}\n${Symbols.LineVertical + Colors.Reset}\n`)
 }
 
 /**
@@ -27,11 +32,15 @@ export function intro(...message: string[]) {
  * outro('Bye world')
  * ```
  */
-export function outro(...message: string[]) {
-  const splittedMessage = message.map(mes => mes.trim().match(regex)!)
-  const largestString = getLargestStringLength(splittedMessage.flatMap(str => str))
+export function outro(title: string, ...message: string[]) {
+  const splittedTitle = title.trim().match(regexTitle)!
 
-  stdout.write(`${Colors.FgGreen + Symbols.MiddleLine + Symbols.LineHorizontal.repeat(largestString + 2) + Symbols.TopRightCorner}\n${splittedMessage.map(msg => msg.map(m => `${Symbols.LineVertical + Colors.Reset} ${Colors.Dim + m.padEnd(largestString, ' ') + Colors.Reset} ${Colors.FgGreen + Symbols.LineVertical}`).join('')).join('\n')}\n${Symbols.BottomLeftCorner + Symbols.LineHorizontal.repeat(largestString + 2) + Symbols.BottomRightCorner + Colors.Reset}\n`)
+  const splittedMessage = message.map(mes => mes.trim().match(regex)!)
+  const largestString = getLargestStringLength([...splittedMessage.flatMap(str => str), ...splittedTitle])
+
+  stdout.write(`${splittedTitle.map((ttl, index) => `${Colors.FgGreen}${index === 0 ? Symbols.MiddleLine : Symbols.LineVertical} ${Colors.Reset + Colors.Reverse} ${ttl} ${Colors.Reset + Colors.FgGreen} ${index === 0 ? Symbols.LineHorizontal.repeat(largestString - ttl.length) + Symbols.TopRightCorner : ' '.repeat(largestString - ttl.length) + Symbols.LineVertical}`).join('')}\n`)
+
+  stdout.write(`${splittedMessage.map(msg => msg.map(m => `${Symbols.LineVertical + Colors.Reset} ${Colors.Dim + m.padEnd(largestString + 2, ' ') + Colors.Reset} ${Colors.FgGreen + Symbols.LineVertical}`).join('')).join('\n')}\n${Symbols.BottomLeftCorner + Symbols.LineHorizontal.repeat(largestString + 4) + Symbols.BottomRightCorner + Colors.Reset}\n`)
 }
 
 /**
@@ -41,9 +50,13 @@ export function outro(...message: string[]) {
  * info('This is an info message')
  * ```
  */
-export function info(...message: string[]) {
-  const splittedMessage = message.map(mes => mes.trim().match(regex)!)
-  const largestString = getLargestStringLength(splittedMessage.flatMap(str => str))
+export function info(title: string, ...message: string[]) {
+  const splittedTitle = title.trim().match(regexTitle)!
 
-  stdout.write(`${Colors.FgGreen + Symbols.MiddleLine + Symbols.LineHorizontal.repeat(largestString + 2) + Symbols.TopRightCorner}\n${splittedMessage.map(msg => msg.map(m => `${Symbols.LineVertical + Colors.Reset} ${Colors.Dim + m.padEnd(largestString, ' ') + Colors.Reset} ${Colors.FgGreen + Symbols.LineVertical}`).join('')).join('\n')}\n${Symbols.MiddleLine + Symbols.LineHorizontal.repeat(largestString + 2) + Symbols.BottomRightCorner}\n${Symbols.LineVertical + Colors.Reset}\n`)
+  const splittedMessage = message.map(mes => mes.trim().match(regex)!)
+  const largestString = getLargestStringLength([...splittedMessage.flatMap(str => str), ...splittedTitle])
+
+  stdout.write(`${splittedTitle.map((ttl, index) => `${Colors.FgGreen}${index === 0 ? Symbols.MiddleLine : Symbols.LineVertical} ${Colors.Reset + Colors.Reverse} ${ttl} ${Colors.Reset + Colors.FgGreen} ${index === 0 ? Symbols.LineHorizontal.repeat(largestString - ttl.length) + Symbols.TopRightCorner : ' '.repeat(largestString - ttl.length) + Symbols.LineVertical}`).join('')}\n`)
+
+  stdout.write(`${splittedMessage.map(msg => msg.map(m => `${Symbols.LineVertical + Colors.Reset} ${Colors.Dim + m.padEnd(largestString + 2, ' ') + Colors.Reset} ${Colors.FgGreen + Symbols.LineVertical}`).join('')).join('\n')}\n${Symbols.MiddleLine + Symbols.LineHorizontal.repeat(largestString + 4) + Symbols.BottomRightCorner}\n${Symbols.LineVertical + Colors.Reset}\n`)
 }
