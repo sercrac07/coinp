b3e2187cc160440baff551687721849fee3f2b80
Sercrac07 <sercassan@gmail.com>
2024-10-03 14:08:32 +0200

    lint: remove some rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,9 @@
-import eslint from "@eslint/js"
-import tseslint from "typescript-eslint"
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
 
-export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended)
+export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
+  rules: {
+    'no-control-regex': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+})
